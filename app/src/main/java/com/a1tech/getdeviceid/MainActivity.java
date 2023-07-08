0cc0f11bdf3144ca39f3aa7c17b671679c8f73ac
dckrOff <azamatotaboev7621@gmail.com>
2023-07-08 17:22:36 +0500

    device id is showed on main screen

--- app/src/main/java/com/a1tech/getdeviceid/MainActivity.java
+++ app/src/main/java/com/a1tech/getdeviceid/MainActivity.java
@@ -1,9 +1,16 @@
 package com.a1tech.getdeviceid;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = findViewById(R.id.txt);
         @SuppressLint("HardwareIds") String IMEI = Settings.Secure.getString(MainActivity.this.getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        Log.e("Deviceid", IMEI);
-
+        textView.setText("Your Device Id:\n" + IMEI);
     }
 }
